09fe09c9e8a2aecf7236d691bd156867ce5bd6c8
jhwinter <14960780+jhwinter@users.noreply.github.com>
2024-05-28 15:41:22 -0400

    finished getting started with junit 5 section

---
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -0,0 +1,47 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GreetingTest {
+    private Greeting greeting;
+
+    @BeforeAll
+    public static void beforeClass() {
+        System.out.println("Before - I am only called once!!!");
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("In Before Each...");
+        greeting = new Greeting();
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In After Each...");
+    }
+
+    @AfterAll
+    public static void afterClass() {
+        System.out.println("After!!! - I am only called once!!!");
+    }
+
+    @Test
+    void helloWorld() {
+        System.out.println(greeting.helloWorld());
+    }
+
+    @Test
+    void helloWorld1() {
+        System.out.println(greeting.helloWorld("John"));
+    }
+
+    @Test
+    void helloWorld2() {
+        System.out.println(greeting.helloWorld("Sam"));
+    }
+}
